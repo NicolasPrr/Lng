f64e160bf9f9f95a35c7f17282ee0865ee4faae8
NicolasPrr <niparrara@unal.edu.co>
2018-03-13 18:38:10 -0500

    Add files via upload

--- Automata.py
+++ Automata.py
@@ -1,4 +1,64 @@
+import array
 import sys
+import graphviz as gv
+
+
+file = open('texto.txt', 'r')
+read_file = file.readlines()
+file.close()
+
+a = []
+transi = []
+
+for i in range(len(read_file)):
+    a.append(read_file[i].strip().split(","))
+
+for i in range(len(a)):
+    if(i==0):
+        Alpha = a[i]
+    if(i==1):
+        estados = a[i]
+    if (i == 2):
+        trans = a[i]
+    if (i == 3):
+        ini = a[i]
+    if (i == 4):
+        final = a[i]
+for i in range(len(trans)):
+    cadena = list(trans[i])
+    transi.append(cadena)
+
+print(a)
+print(estados)
+print(transi)
+print(ini)
+print(final)
+
+
+def draw(alafabeto, estados, inicial, transiccion, final):
+    print("inicio:", str(inicial))
+    g = gv.Digraph(format='svg')
+    g.graph_attr['rankdir'] = 'LR'
+    g.node('ini', shape="point")
+    for e in estados:
+        if e in final:
+            g.node(e, shape="doublecircle")
+        else:
+            g.node(e)
+        if e in inicial:
+            g.edge('ini', e)
+
+    for t in transiccion:
+        if t[2] not in alafabeto:
+            return 0
+        g.edge(t[0], t[1], label=str(t[2]))
+    g.render(view=False )
+
+
+
+#draw(Alpha, estados, ini, transi, final)
+
+
 def printMatriz(Matriz):
     for i in range(len(Matriz)):
         print(*Matriz[i])
@@ -7,36 +67,39 @@ def printMatriz(Matriz):
 def getW(estado, letra, matriz, alpha):
     #La funcion nos retorna  el estado dada la tabla de transicion en el estado "estado" para una entrada "letra" en el alfabeto
     #alpha
+    j = int(estado)
     pos = 0
     for i in range(len(alpha)):
-        if alpha[i] == letra :
-            return matriz[estado][i]
+        if alpha[i] == letra:
+            return matriz[j][i]
 
 
-print("Ingrese el alfabeto separado por espacio")
-Alpha=(sys.stdin.readline().strip().split(" "))
-print("Ingrese el numero de estados")
-T = int(sys.stdin.readline())
+print("Alfabeto:")
+print(Alpha)
+print("Estados")
+print(len(estados))
 print("Los estados son los elementos: ")
-print(*range(T))
-Matriz =  []
-for i in range(T):
+for j in range(len(estados)):
+    print(estados[j])
+
+Matriz = []
+for i in range(len(estados)):
     Matriz.append([i])
     for j in range(len(Alpha) - 1):
         Matriz[i].append(None)
 
 
-for i in range(T):
-    print("ingrese las transiciones para el estado " + str(i))
-    for j in range(len(Alpha)):
-        print("ingrese el estado transicion para  " + str(Alpha[j]))
-        S = int(sys.stdin.readline())
-        Matriz[i][j] = S
+for t in transi:
+    i = int(t[0])
+    aux = t[2]
+    j = Alpha.index(aux)
+    S = t[1]
+    Matriz[i][j] = S
 
 printMatriz(Matriz)
-print("Ingrese los estados finales")
-estadosf=(sys.stdin.readline().strip().split(" "))
-estadoa = 0
+print("Estado final")
+for i in range (len(final)):
+    print(final[i])
 
 
 print("Ingrese el numero de pruebas ")
@@ -47,12 +110,13 @@ for i in range(N):
     estadoa = 0
     print("Estado INICIAL q_" + str(estadoa))  # ESTADO INICIAL
     for j in range(len(S)):
-        estadoa = getW(estadoa,S[j], Matriz, Alpha)
+        estadoa = getW(estadoa, S[j], Matriz, Alpha)
         print("Estado actual q_" + str(estadoa)) #RECORRIDO DE ESTADOS
 
-
-    if str(estadoa) in estadosf : #se hace el cast porque estadosf los reconoce como strings
+    if str(estadoa) in final : #se hace el cast porque estadosf los reconoce como strings
         print("ACEPTA LA PALARBA: " + S.strip())
     else:
         print("NO LO ACEPTA: " + S)
+
+
 
